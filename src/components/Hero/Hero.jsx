fa6fb053164e667831e7ad47f7bc04ad2ca8ce54
Nik Harith <2673209b@student.gla.ac.uk>
2024-08-11 14:33:43 +0100

    Hero section done!

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from '../../utils'
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Neeha</h1>
+        <p className={styles.description}>I'm mostly an AR developer with experiences working with many AR tools. 
+          Reach me out for any ideas or works!</p>
+        <a href='mailto:nik@neeha.my' className={styles.contactBtn}>HMU!</a>
+      </div>
+      <img src={getImageUrl('hero/heroImage.png')} alt='Me img' className={styles.heroImg}></img>
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  )
+}
